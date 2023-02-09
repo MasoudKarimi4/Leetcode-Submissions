b46f152dfcae394127025b7e76fbeb05ee84910a
Masoud Karimi <74917502+MasoudKarimi4@users.noreply.github.com>
2023-02-09 13:37:52 -0500

    Time: 75 ms (66.42%), Space: 14.3 MB (51.13%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,16 +1,17 @@
 class Solution(object):
     def twoSum(self, nums, target):
 
-        answerList = []
-        tested = False 
-
+        table = {}
+        sums = []
+        
         for i in range(len(nums)):
-            for g in range(len(nums)):
-                if nums[i] + nums[g] == target and i != g and tested == False:
-                    answerList.append(i)
-                    answerList.append(g)
-                    tested = True
-                    break
-    
+            
 
-        return answerList 
+            number = target - nums[i]
+            print(number)
+
+            if number in table:
+                return (table[number],i)
+            else:
+                table[nums[i]] = i
+        
