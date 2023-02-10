cc7ed8bcacaa12fee38d6c72362a15556c272b85
Masoud Karimi <74917502+MasoudKarimi4@users.noreply.github.com>
2023-02-10 13:11:27 -0500

    Time: 105 ms (37.03%), Space: 13.7 MB (17.12%) - LeetHub

---
+++ 0326-power-of-three/0326-power-of-three.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def isPowerOfThree(self, n):
+        
+        
+        
+        
+        for i in range(30):
+            if 3**i == n:
+                return True
+        return False 
+        
+        
+        
+        
